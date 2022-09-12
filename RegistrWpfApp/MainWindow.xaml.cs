b7d175963cc2941e2ef5031332e9f1b680497d2c
sma888 <magamet.sitimov@gmail.com>
2022-09-12 08:44:38 +0300

    first commit

---
+++ RegistrWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RegistrWpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            string login, password, passwordBoxText;
+
+            login = loginTextBox.Text;
+            password = passwordTextBox.Text;
+            passwordBoxText = passwordBox.Password.ToString();
+
+            VerificateUser verificateUser = new VerificateUser();
+            bool result = verificateUser.Check(login, password, passwordBoxText);
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordTextBox.Visibility = Visibility.Visible;
+            passwordTextBox.Text = passwordBox.Password.ToString();
+            passwordBox.Visibility = Visibility.Hidden;
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.Visibility = Visibility.Visible;
+            passwordBox.Password = passwordTextBox.Text;
+            passwordTextBox.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ RegistrWpfApp/VerificateUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RegistrWpfApp
+{
+    public class VerificateUser
+    {
+
+        //public string Login { get; set; }
+        //public string Password { get; set; }
+
+        //public VerificateUser(string login, string password)
+        //{
+        //    Login = login;
+        //    Password = password;
+        //}
+
+        public bool Check(string login, string password, string passwordBoxText)
+        {
+            if (login != "" || password != ""|| passwordBoxText != "")
+            {
+                if (login == "Мага" && password == "123"|| passwordBoxText == "123")
+                {
+                    MessageBox.Show("Вход выполнен успешно", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Stop);
+                    return false;
+                }
+            }
+            else MessageBox.Show("Не все поля заполнены!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Stop);
+            return false;
+        }
+    }
+}
