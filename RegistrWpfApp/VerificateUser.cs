f1a5314f21d29778062563667079108b4ae5e813
sma888 <magamet.sitimov@gmail.com>
2022-09-22 22:11:38 +0300

    second commit

--- RegistrWpfApp/VerificateUser.cs
+++ RegistrWpfApp/VerificateUser.cs
@@ -1,40 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.ObjectModel;
 
 namespace RegistrWpfApp
 {
     public class VerificateUser
     {
 
-        //public string Login { get; set; }
-        //public string Password { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
 
-        //public VerificateUser(string login, string password)
-        //{
-        //    Login = login;
-        //    Password = password;
-        //}
+
+        public static ObservableCollection<VerificateUser> GetUsersList()
+        {
+
+            var userList = new ObservableCollection<VerificateUser>()
+            {
+                new VerificateUser() { Login = "Мага", Password = "123" },
+            };
+
+            return userList;
+        }
+
 
         public bool Check(string login, string password, string passwordBoxText)
         {
-            if (login != "" || password != ""|| passwordBoxText != "")
+            foreach (var user in GetUsersList())
             {
-                if (login == "Мага" && password == "123"|| passwordBoxText == "123")
-                {
-                    MessageBox.Show("Вход выполнен успешно", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
-                    return true;
-                }
-                else
-                {
-                    MessageBox.Show("Неверный логин или пароль", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Stop);
-                    return false;
-                }
+                if (user.Login == login && user.Password == password) return true;
+                else return false;
             }
-            else MessageBox.Show("Не все поля заполнены!", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Stop);
             return false;
         }
     }
